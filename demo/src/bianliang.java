e7871dbbf0eb104de34ed2dd36e9f1a267ddcdae
jeff <tae44@vip.qq.com>
2017-08-02 21:05:50 +0800

    bianliang

---
+++ demo/src/bianliang.java
@@ -0,0 +1,25 @@
+public class bianliang {
+    public static void main(String[] args) {
+        byte b1 = 127;
+//        byte b2 = 128;
+
+        long num1 = 1234567890;
+        long num2 = 12345678900L;
+
+        float f = 9.0f;
+        double d = 9.0;
+
+        double d1 = 2.6;
+        d1 -= 2;
+        System.out.println(d1);
+
+        char c = '\\';
+        System.out.println(c);
+
+        char c1 = 65;
+        System.out.println(c1);
+
+        char c2 = '\uffff';
+        System.out.println(c2);
+    }
+}
