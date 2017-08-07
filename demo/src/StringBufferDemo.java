fda4d5b659d59faa3c474a36ce6055dbf5f9b91c
jeff <tae44@vip.qq.com>
2017-08-07 22:51:03 +0800

    java StringBuffer

---
+++ demo/src/StringBufferDemo.java
@@ -0,0 +1,50 @@
+public class StringBufferDemo {
+    public static void main(String[] args) {
+        // 添加();
+        // 插入();
+        // 删除();
+        // 替换();
+    }
+
+    public static void 替换() {
+        StringBuffer sb = new StringBuffer("abcd");
+        sb.replace(1, 3, "qq"); // 范围含头不含尾
+        System.out.println(sb);
+    }
+
+    public static void 删除() {
+        StringBuffer sb = new StringBuffer("abcd");
+        // sb.delete(1, 3); 删除
+        // sb.delete(0, sb.length()); 清空
+        // sb = new StringBuffer(); 清空,不建议使用
+        sb.deleteCharAt(2);
+        System.out.println(sb);
+    }
+
+    public static void 插入() {
+        StringBuffer sb = new StringBuffer("abcd");
+        sb.insert(2, "qq");
+        System.out.println(sb);
+    }
+
+    public static void 添加() {
+        StringBuffer sb = new StringBuffer();
+        StringBuffer sb1 = sb.append(5);
+        System.out.println(sb1);
+        System.out.println(sb);
+        System.out.println(sb == sb1);
+
+//        String s = "abc";
+//        String s1 = s + "xxoo";
+//        System.out.println(s == s1);
+        /*
+            在进行频繁的字符串拼接操作时,可以使用StringBuffer
+            因为使用String每进行一次拼接操作,就会创建一个字符串对象,这会影响内存,而StringBuffer不会
+         */
+
+        sb.append(5);
+        sb.append("hello");
+        System.out.println("sb: " + sb);
+        System.out.println("sb1: " + sb1);
+    }
+}
