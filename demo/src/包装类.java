1a3108393a76bb3c075099646ba2453aeb1e0164
syjiang <tae44@vip.qq.com>
2017-08-08 18:14:20 +0800

    java

--- demo/src/包装类.java
+++ demo/src/包装类.java
@@ -1,5 +1,30 @@
 public class 包装类 {
     public static void main(String[] args) {
+        // method1();
+        // 进制转换();
+        Integer a = new Integer("3");
+        Integer b = new Integer(3);
+        System.out.println(a == b);
+        System.out.println(a.equals(b));
+    }
+
+    private static void 进制转换() {
+        // 十进制 --> 其他进制
+        System.out.println(Integer.toBinaryString(60));     // 二进制
+        System.out.println(Integer.toOctalString(60));      // 八进制
+        System.out.println(Integer.toHexString(60));        // 十六进制
+        System.out.println(Integer.toString(60, 2));  // 二进制
+        System.out.println(Integer.toString(60, 8));  // 八进制
+        System.out.println(Integer.toString(60, 16)); // 十六进制
+        System.out.println(Integer.toString(60, 3));  // 三进制
+
+        // 其他进制 --> 十进制
+        System.out.println(Integer.parseInt("111100", 2));
+        System.out.println(Integer.parseInt("74", 8));
+        System.out.println(Integer.parseInt("3C", 16));
+    }
+
+    private static void method1() {
         int max = Integer.MAX_VALUE;
         int min = Integer.MIN_VALUE;
         System.out.println(max);
@@ -18,6 +43,8 @@ public class 包装类 {
         s = String.valueOf(1);
         s = Integer.toString(2);
 
+        // 把字符串封装成Integer对象,Integer对象调用intValue()方法转成int类型
         Integer i1 = new Integer("123");
+        int i2 = i1.intValue();
     }
 }
