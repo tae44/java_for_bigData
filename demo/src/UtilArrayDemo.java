2279950799f8d98d5989b63e2a57bf081f939901
jeff <tae44@vip.qq.com>
2017-08-12 17:00:28 +0800

    java

---
+++ demo/src/UtilArrayDemo.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class UtilArrayDemo {
+    public static void main(String[] args) {
+        // fill();
+        String[] arr = {"abc", "def", "xxoo"};
+        List<String> list = Arrays.asList(arr);
+        System.out.println(list.contains("abc"));
+        List<String> list1 = new ArrayList(list);
+        System.out.println(list1);
+        list1.add("kkk");
+        System.out.println(list1);
+    }
+
+    public static void fill() {
+        int[] arr = new int[5];
+        // 设值
+        Arrays.fill(arr, 8);
+        System.out.println(Arrays.toString(arr));
+    }
+}
--- demo/src/UtilDemo.java
+++ demo/src/UtilDemo.java
@@ -1,6 +1,56 @@
+import java.util.*;
+
 public class UtilDemo {
     public static void main(String[] args) {
         // 给可重复的集合排序 -- List
+        List<String> list = new ArrayList<>();
+        list.add("asd");
+        list.add("ruyif");
+        list.add("oo");
+        list.add("z");
+        list.add("qwertvc");
+        list.add("vfss");
+        list.add("bbnlk");
+        Comparator<String> c = new Comparatorbylength();
+        System.out.println("排序之前: " + list);
+        // method1(list);
+        // method2(list, c);
+        // 反转指定列表中元素的顺序
+        // Collections.reverse(list);
+        // 替换元素
+        // Collections.replaceAll(list, "asd", "@@@");
+        // 把所有元素替换为指定的元素
+        // Collections.fill(list, "xxoo");
+        // 打乱顺序
+        Collections.shuffle(list);
+        System.out.println("排序之后: " + list);
+    }
 
+    public static void method2(List<String> list, Comparator c) {
+//        Collections.sort(list, c);
+//        String min = Collections.min(list, c);
+//        System.out.println("min=" + min);
+//        String max = Collections.max(list, c);
+//        System.out.println("max=" + max);
+
+        // 返回一个比较器,强行逆转指定比较强的顺序
+        Comparator<String> cc = Collections.reverseOrder(c);
+        Collections.sort(list, cc);
+    }
+
+    public static void method1(List<String> list) {
+        Collections.sort(list);
+        String min = Collections.min(list);
+        System.out.println("min=" + min);
+        String max = Collections.max(list);
+        System.out.println("max=" + max);
+    }
+}
+
+class Comparatorbylength implements Comparator<String> {
+    @Override
+    public int compare(String o1, String o2) {
+        int temp = o1.length() - o2.length();
+        return temp == 0 ? o1.compareTo(o2) : temp;
     }
 }
