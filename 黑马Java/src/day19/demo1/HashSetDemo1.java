5f03087d3b2b19e952cd7358016a5f1483a2d116
syjiang <tae44@vip.qq.com>
2017-09-22 09:12:15 +0800

    黑马Java day19

---
+++ 黑马Java/src/day19/demo1/HashSetDemo1.java
@@ -0,0 +1,32 @@
+package day19.demo1;
+
+import day15.demo01.Person;
+import java.util.HashSet;
+
+/*
+ *  HashSet集合的自身特点:
+ *    底层数据结构,哈希表
+ *    存储,取出都比较快
+ *    线程不安全,运行速度快
+ */
+public class HashSetDemo1 {
+	public static void main(String[] args) {
+		/*HashSet<String> set = new HashSet<String>();
+		set.add(new String("abc"));
+	    set.add(new String("abc"));
+		set.add(new String("bbc"));
+		set.add(new String("bbc"));
+		System.out.println(set);*/
+
+		//将Person对象中的姓名,年龄,相同数据,看作同一个对象
+		//判断对象是否重复,依赖对象自己的方法 hashCode,equals
+		HashSet<Person> setPerson = new HashSet<Person>();
+		setPerson.add(new Person("a",11));
+		setPerson.add(new Person("b",10));
+		setPerson.add(new Person("b",10));
+		setPerson.add(new Person("c",25));
+		setPerson.add(new Person("d",19));
+		setPerson.add(new Person("e",17));
+		System.out.println(setPerson);
+	}
+}
