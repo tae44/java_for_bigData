ec2ab882563994f25f196c3b732c32a22a4068f1
syjiang <tae44@vip.qq.com>
2017-09-27 17:10:33 +0800

    黑马Java day23

--- 黑马Java/src/day23/练习/Test.java
+++ 黑马Java/src/day23/练习/Test.java
@@ -1,28 +1,21 @@
 package day23.练习;
 
-import java.io.FileOutputStream;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.StreamCorruptedException;
 
 public class Test {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         f();
     }
 
-    public static void f() {
-        FileOutputStream fos = null;
-        try {
-            fos = new FileOutputStream("E:\\abc.txt");
-            fos.write(97);
-        } catch (IOException e) {
-            throw new RuntimeException("文件写入失败,请重试");
-        } finally {
-            if (fos != null) {
-                try {
-                    fos.close();
-                } catch (IOException e) {
-                    throw new RuntimeException("关闭资源失败");
-                }
-            }
+    public static void f() throws IOException {
+        FileInputStream fis = new FileInputStream("E:\\aaa.txt");
+        byte[] b = new byte[1024];
+        int i;
+        while ((i = fis.read(b)) != -1) {
+            System.out.print(new String(b, 0, i));
         }
+        fis.close();
     }
 }
