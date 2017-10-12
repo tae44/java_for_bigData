c89a91635890236008fee201bd2ee2acccf663ff
syjiang <tae44@vip.qq.com>
2017-10-12 16:32:26 +0800

    黑马Java day30

---
+++ 黑马Java/src/day30/demo1/TestJDBCUtils.java
@@ -0,0 +1,12 @@
+package day30.demo1;
+
+import java.sql.Connection;
+
+import cn.itcast.jdbcutil.JDBCUtilsConfig;
+
+public class TestJDBCUtils {
+	public static void main(String[] args) {
+		Connection con = JDBCUtilsConfig.getConnection();
+		System.out.println(con);
+	}
+}
---
+++ 黑马Java/src/day30/练习/Test.java
@@ -0,0 +1,7 @@
+package day30.练习;
+
+public class Test {
+    public static void main(String[] args) {
+
+    }
+}
